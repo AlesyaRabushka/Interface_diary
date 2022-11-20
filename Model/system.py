2bc2c6fa2ec89a55d7a5e665b786b6f683f75702
Aleksa <72038770+AlesyaRabushka@users.noreply.github.com>
2022-11-20 16:28:59 +0300

    done with screens

--- Model/system.py
+++ Model/system.py
@@ -1,14 +1,86 @@
 from View.view import *
 
-
-
 class System:
+    """
+    Система
+    """
     def __init__(self):
-        # self.sm = ScreenManager()
         pass
-        # self.sm.add_widget(self.main_screen)
+
+    #
+    def authorization(self):
+        pass
+    #
+    def open_menu(self):
+        pass
+    #
+    def add_new_user(self, name : str):
+        pass
+    #
+    def add_new_record(self):
+        pass
+    #
+    def remove_record(self):
+        pass
+    #
+    def edit_record(self, date: str, time:str,name:str):
+        pass
+    #
+    def update_record(self):
+        pass
 
 
+class User:
+    """
+    Пользователь
+    """
+    def __init__(self):
+        # имя
+        self.name = ''
+        # фотография
+        self.photo_path = ''
 
-    def get_screen(self):
-        return self.sm
+    # ДобавитьЗапись(Запись запись)
+    def add_record(self, record):
+        pass
+    # УдалитьЗапись(Запись запись)
+    def remove_record(self, record):
+        pass
+    # РедактироватьЗапись(Запись запись)
+    def edit_record(self, record):
+        pass
+    # ПросмотретьЗапись(Запись запись)
+    def read_record(self, record):
+        pass
+
+
+class Registration:
+    """
+    Регистрация
+    """
+    def __init__(self):
+        # ФИО
+        self.fio = ''
+        # пароль
+        self.password = ''
+        # логин
+        self.login = ''
+        # фотография
+        self.photo_path = ''
+
+    # регистрация()
+    def registration(self):
+        pass
+
+
+class Validate:
+    """
+    Проверка
+    """
+    def __init__(self):
+        # списокЛогинов
+        self.login_list = Validate()
+
+    # проверка(string логин, string пароль)
+    def validate(self, login:str, password:str)-> bool:
+        pass
--- View/view.py
+++ View/view.py
@@ -1,8 +1,10 @@
-
 from kivymd.uix.screen import MDScreen
+#from Model.system import User, Registration, Validate
 from kivymd.uix.datatables import MDDataTable
 from kivy.metrics import dp
 from kivy.uix.popup import Popup
+from kivymd.uix.tooltip import MDTooltip
+from kivy.uix.button import Button
 from kivy.factory import Factory
 from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import ScreenManager, Screen
@@ -10,7 +12,8 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 class WindowManager(ScreenManager):
     pass
 
-
+class TooltipButton(Button, MDTooltip):
+    pass
 # class WelcomeScreenView(Popup, Widget):
 #     def __init__(self, system, **kwargs):
 #         super().__init__(**kwargs)
@@ -35,25 +38,51 @@ class MainScreenView(MDScreen):
         #                              ("Дата последнего приема", dp(30)),
         #                              ("ФИО ветеринара", dp(30)),
         #                              ("Диагноз", dp(30))], size_hint=(1, 0.7))
-        #                          # row_data=self.add_table_data())
-        # self.table.bind(on_row_press=self.pet_info_window)
-        # self.table.bind(on_check_press=self.check_info)
-
+        # #                          # row_data=self.add_table_data())
+        # # self.table.bind(on_row_press=self.pet_info_window)
+        # # self.table.bind(on_check_press=self.check_info)
+        #
         # self.add_widget(self.table)
 
 
 class RegistrationScreenView(MDScreen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        # self.system = system
+        # ФИО
+        self.fio = ''
+        # пароль
+        self.password = ''
+        # логин
+        self.login = ''
 
-    def return_system(self):
-        return self.system
+        # новыйПользователь
+        # self.registration = Registration()
+
+    # сохранить(ФИО)
+    def save(self, fio):
+        pass
+    # регистрация()
+    def registration(self):
+        pass
+
+
 #
 class AuthentificationScreenView(MDScreen):
+    """
+    ОкноВход
+    """
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        # self.system = system
+        # пароль
+        self.password = ''
+        # логин
+        self.login = ''
+
+    def set_login(self, login):
+        self.login = login
+    def set_password(self, password):
+        self.password = password
+
 
     def return_system(self):
         return self.system
@@ -67,7 +96,17 @@ class ForgetPasswordScreenView(MDScreen, Screen):
 class AccountScreenView(MDScreen, Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        # self.system = system
+        # ФИО
+        self.fio = ''
+        # фотография
+        self.photo_path = ''
+        # почта
+        self.email =''
+
+        # проверкаПользователя
+        # self.validation = Validate()
+        # модель
+        # self.model = User()
 
 
 class EditAccountScreenView(MDScreen):
@@ -76,9 +115,34 @@ class EditAccountScreenView(MDScreen):
         # self.system = system
 
 class RecordsScreenView(MDScreen):
+    """
+    ОкноЗадачи
+    """
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        # запись
+        #self.record = ??????
+        # self.table = MDDataTable(pos_hint={'center_y': 0.58, 'center_x': 0.6},
+        #                          use_pagination=True,
+        #                          check=True,
+        #                          column_data=[
+        #                              ("Имя питомца", dp(40)),
+        #                              ("Вид животного", dp(30)),
+        #                              ("Дата рождения", dp(30)),
+        #                              ("Дата последнего приема", dp(30)),
+        #                              ("ФИО ветеринара", dp(30)),
+        #                              ("Диагноз", dp(30))], size_hint=(0.75, 0.7))
+        #
+        # self.add_widget(self.table)
+
+    # сохранить()
+    def save(self):
         pass
+
+
+class SettingsScreenView(MDScreen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
 
 class ProgressScreenView(MDScreen):
@@ -86,17 +150,18 @@ class ProgressScreenView(MDScreen):
         super().__init__(**kwargs)
         pass
 
-class NotificationsScreenView(MDScreen):
+class NotificationsScreenView(Popup, Widget):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         pass
 
-class AddRecordScreenView(MDScreen):
+
+class AddRecordScreenView(Popup, Widget):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        pass
 
-class RemoveRecordScreenView(MDScreen):
+
+class RemoveRecordScreenView(Popup, Widget):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         pass
@@ -106,7 +171,7 @@ class RemoveAccountScreenView(MDScreen):
         super().__init__(**kwargs)
         pass
 
-class EditRecordScreenView(MDScreen):
+class EditRecordScreenView(Popup, Widget):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         pass
@@ -117,4 +182,17 @@ class ExitScreenView(MDScreen):
         super().__init__(**kwargs)
 
 
+class ErrorScreenView(Popup, Widget):
+    """
+    Ошибка
+    """
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # НаименованиеОшибки
+        self.error_name = ''
+
+
+class RecordMenuScreenView(Popup, Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
